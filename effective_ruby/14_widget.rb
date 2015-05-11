38c3aa01705ad11e7fa525c2f28ddbd205384f9e
misogi <dev@misogi.info>
2015-05-11 15:54:15 +0900

    14 protected を使えば同じクラスの別院スタンスから呼べる

--- effective_ruby/14_widget.rb
+++ effective_ruby/14_widget.rb
@@ -1,6 +1,17 @@
 class Widget
- def overlapping?(other)
-   x1, y1 = @screen_x, @screen_y
-   x2, y2 = other.instance_eval {[@screen_x, @screen_y]}
- end
+  def initialize(x, y)
+    @screen_x = x
+    @screen_y = y
+  end
+
+  def overlapping?(other)
+    x1, y1 = screen_coordinates
+    x2, y2 = other.screen_coordinates
+  end
+  
+  protected
+  
+  def screen_coordinates
+    [@screen_x, @screen_y]
+  end
 end
