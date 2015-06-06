259193978c3d5fb076d416dd4819fca52ea0d19d
misogi <dev@misogi.info>
2015-06-06 19:37:46 +0900

    38 mock

---
+++ effective_ruby/38_monitor_test.rb
@@ -0,0 +1,30 @@
+require './38_mock'
+
+class MonitorTest < Minitest::Test
+  def test_successful_monitor
+    monitor = Monitor.new('example.com')
+    response = MiniTest::Mock.new
+
+    monitor.define_singleton_method(:get) do |echo|
+      response.expect(:success?, true)
+      response.expect(:body, echo)
+      response
+    end
+
+    assert(monitor.alive?, 'should be alive')
+    response.verify
+  end
+
+  def test_failed_monitor
+    monitor = Monitor.new 'example.com'
+    response = MiniTest::Mock.new
+
+    monitor.define_singleton_method(:get) do |echo|
+      response.expect(:success?, false)
+      response
+    end
+
+    refute(monitor.alive?)
+    response.verify
+  end
+end
